039653ad011efb3c5115bae57c96c5ba681fc101
thienjs <thienjsx@gmail.com>
2021-11-22 13:47:26 -0500

    createCheckoutSession Function

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -1,5 +1,37 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
-  const { item, email } = req.body;
+  const { items, email } = req.body;
+
+  console.log(items);
+
+  const transformedItems = items.map((item) => ({
+    price_data: {
+      currency: "usd",
+      product_data: {
+        images: [item.image],
+        name: item.title,
+      },
+      unit_amount: item.price * 100,
+    },
+    description: item.description,
+    quantity: 1,
+  }));
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    shipping_address_collection: {
+      allowed_countries: ["US"],
+    },
+    line_items: transformedItems,
+    mode: "payment",
+    success_url: "http://localhost:3000/success",
+    cancel_url: "http://localhost:3000/cancel",
+    metadata: {
+      email,
+      images: JSON.stringify(items.map((item) => item.image)),
+    },
+  });
+
+  res.status(200).json({ id: session.id });
 };
